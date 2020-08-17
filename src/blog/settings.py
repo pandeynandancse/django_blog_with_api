122e6e2ad7e7569279d1a7fdd3c9ce4b85d2dcba
Nandan Pandey <pandeynandancse@gmail.com>
2020-08-17 04:50:48 +0000

    Update settings.py

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-blog-api.herokuapp.com"]
+ALLOWED_HOSTS = ["django-blog-with-api.herokuapp.com"]
 
 
 # Application definition
